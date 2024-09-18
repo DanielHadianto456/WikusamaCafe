0d772dda26ff866fe543273bbe675b3490da0256
Daniel Hadianto <daniel88hadianto@gmail.com>
2024-09-18 14:56:07 +0700

    Feat: Added Food Controllers for Admin

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\Admin\FoodController;
 
 //API Route Collection for Authentication Taken from AuthController Controller
 Route::middleware('auth:user_model')->group(function () {
+
     Route::controller(AuthController::class)->group(function (){
         Route::post('/auth/logout', 'logout');
+    });
+
+    //Admin API
+    Route::controller(FoodController::class)->group(function (){
+        Route::get('/admin/food/get', 'getFood');
+        Route::get('/admin/food/getId/{id}', 'getFoodId');
+        Route::post('/admin/food/add', 'addFood');
+        Route::patch('/admin/food/update/{id}', 'updateFood');
+        Route::delete('/admin/food/delete/{id}', 'deleteFood');
     });
 });
 
