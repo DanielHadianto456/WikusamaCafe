722feab3c6ecc1204c0fbca832292517957d8a7f
Daniel Hadianto <daniel88hadianto@gmail.com>
2024-09-30 18:44:37 +0700

    Feat: Added Dashboard for Cashier

--- resources/js/components/Kasir/KasirPanel.vue
+++ resources/js/components/Kasir/KasirPanel.vue
@@ -1,7 +1,26 @@
 <template>
   <Header />
   <div class="wrapper">
-
+    <div class="card">
+      <h1>Welcome to Cashier Panel</h1>
+    </div>
+    <div class="card buttons-container">
+      <router-link to="/kasir/penjualan" class="button">
+        <strong>
+          History
+        </strong>
+      </router-link>
+      <router-link to="/kasir/penjualan" class="button">
+        <strong>
+          Tambah Pesanan
+        </strong>
+      </router-link>
+      <router-link to="/kasir/penjualan" class="button">
+        <strong>
+          Cetak Nota
+        </strong>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -19,9 +38,48 @@ export default {
 <style scoped>
 .wrapper {
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  background-color: #fff8e8;
+}
+
+.button {
+  display: flex;
   align-items: center;
   justify-content: center;
-  height: 100vh;
-  background-color: #FFF8E8;
+  flex-basis: 25%;
+  height: 7.5vh;
+  cursor: pointer;
+  background-color: #071013;
+  border: none;
+  color: white;
+  border-radius: 5vh;
+  font-size: 2vh;
+  text-decoration: none;
+  font-family: "poppins";
+  margin: 1vh; 
+}
+
+.buttons-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center; 
+  gap: 5vh; 
+}
+
+.wrapper .card {
+  margin-top: 7.5vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "poppins";
+  background: white;
+  border-radius: 10px;
+  box-shadow: 5.5px 8px 8px #00000059;
+  padding: 10vh 10vh 10vh 10vh;
+  height: auto;
+  width: 100vh;
+
 }
 </style>
