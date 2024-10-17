b6d0f18a5b1912739f80a1d18a4a0dc91bf6bc53
Daniel Hadianto <daniel88hadianto@gmail.com>
2024-10-17 13:33:08 +0700

    Feat: Added Quantity Count for Invoices

--- app/Http/Controllers/pdfController.php
+++ app/Http/Controllers/pdfController.php
@@ -10,33 +10,74 @@ use Illuminate\Support\Facades\Log;
 
 class pdfController extends Controller
 {
+    // public function getPdf($id)
+    // {
+    //     $transaction = transaksiModel::with([
+    //         'detailPegawai',
+    //         'detailMeja',
+    //         'detailTransaksi.detailMenu',
+    //     ])->find($id);
+
+    //     if (!$transaction) {
+    //         return response()->json(['message' => 'Transaction not found'], 404);
+    //     }
+
+    //     $totalHarga = $transaction->detailTransaksi->sum('harga');
+
+    //     $data = [
+    //         'title' => 'GrimmCafe Reciept',
+    //         'date' => date('m/d/Y'),
+    //         'transaction' => $transaction,
+    //         'totalHarga' => $totalHarga
+    //     ];
+
+    //     try {
+    //         $pdf = Pdf::loadView('invoice.generate-invoice-pdf', $data);
+    //         return $pdf->download('invoice.pdf');
+    //     } catch (\Exception $e) {
+    //         Log::error('PDF generation error: ' . $e->getMessage());
+    //         return response()->json(['message' => 'Error generating PDF'], 500);
+    //     }
+    // }
+
     public function getPdf($id)
-    {
-        $transaction = transaksiModel::with([
-            'detailPegawai',
-            'detailMeja',
-            'detailTransaksi.detailMenu',
-        ])->find($id);
+{
+    $transaction = transaksiModel::with([
+        'detailPegawai',
+        'detailMeja',
+        'detailTransaksi.detailMenu',
+    ])->find($id);
 
-        if (!$transaction) {
-            return response()->json(['message' => 'Transaction not found'], 404);
-        }
-
-        $totalHarga = $transaction->detailTransaksi->sum('harga');
-
-        $data = [
-            'title' => 'GrimmCafe Reciept',
-            'date' => date('m/d/Y'),
-            'transaction' => $transaction,
-            'totalHarga' => $totalHarga
-        ];
-
-        try {
-            $pdf = Pdf::loadView('invoice.generate-invoice-pdf', $data);
-            return $pdf->download('invoice.pdf');
-        } catch (\Exception $e) {
-            Log::error('PDF generation error: ' . $e->getMessage());
-            return response()->json(['message' => 'Error generating PDF'], 500);
-        }
+    if (!$transaction) {
+        return response()->json(['message' => 'Transaction not found'], 404);
     }
+
+    $totalHarga = $transaction->detailTransaksi->sum('harga');
+
+    // Calculate the quantity of each item
+    $groupedItems = $transaction->detailTransaksi->groupBy('id_menu')->map(function ($items) {
+        $firstItem = $items->first();
+        return [
+            'nama_menu' => $firstItem->detailMenu->nama_menu,
+            'harga' => $firstItem->harga,
+            'quantity' => $items->count(),
+        ];
+    });
+
+    $data = [
+        'title' => 'GrimmCafe Receipt',
+        'date' => date('m/d/Y'),
+        'transaction' => $transaction,
+        'totalHarga' => $totalHarga,
+        'groupedItems' => $groupedItems,
+    ];
+
+    try {
+        $pdf = Pdf::loadView('invoice.generate-invoice-pdf', $data);
+        return $pdf->download('invoice.pdf');
+    } catch (\Exception $e) {
+        Log::error('PDF generation error: ' . $e->getMessage());
+        return response()->json(['message' => 'Error generating PDF'], 500);
+    }
+}
 }
